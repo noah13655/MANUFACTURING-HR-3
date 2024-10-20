1cea09486b713a119d77994c71ea7184543e7a34
JL1365 <borlagdatanjohnlloyd0@gmail.com>
2024-10-20 20:40:38 +0800

    dada

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import {create} from 'zustand'
 import axios from 'axios'
 
 const API_URL = process.env.NODE_ENV === "production"
-  ? "https://manufacturing-hr-3-1.onrender.com/api/auth"
+  ? "https://backend-manufacturing-hr-3.onrender.com/api/auth"
   : "http://localhost:7687/api/auth";
 
 //  const API_URL = "https://backend-hr3.jjm-manufacturing.com/api/auth";
