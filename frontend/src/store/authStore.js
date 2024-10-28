4600da66c574bea94cdccd05a07d27b2ae56954c
JL1365 <borlagdatanjohnlloyd0@gmail.com>
2024-10-28 20:23:14 +0800

    update

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import {create} from 'zustand'
 import axios from 'axios'
 
-const API_URL = import.meta.env.MODE === "production" ? "http://localhost:7687/api/auth" : "/api/auth";
+const API_URL = import.meta.env.NODE_ENV === "production" ? "http://localhost:7687/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
