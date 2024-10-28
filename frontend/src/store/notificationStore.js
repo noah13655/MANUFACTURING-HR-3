1ef021cfc1b2f3b2621ca5755717aa98c660a86b
JL1365 <borlagdatanjohnlloyd0@gmail.com>
2024-10-28 17:21:32 +0800

    Merge branch 'main' of https://github.com/noah13655/MANUFACTURING-HR-3

--- frontend/src/store/notificationStore.js
+++ frontend/src/store/notificationStore.js
@@ -1,7 +1,12 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:7687/api/notification" : "/api/notification";
+const API_URL = 
+  import.meta.env.NODE_ENV === "production" && import.meta.env.RENDER_ENV !== "true"
+    ? "https://backend-hr3.jjm-manufacturing.com/api/notification"
+    : import.meta.env.RENDER_ENV === "true"
+      ? "https://hr3-jjm-manufacturing-1p4f.onrender.com/api/notification"
+      : "http://localhost:7687/api/notification";
 
 axios.defaults.withCredentials = true;
 
